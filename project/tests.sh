3a1f8ccd0edc464089022e638e27c8dc2a8651ed
Nezhdat Dzhefer <nezhdat.dzhefer@fau.de>
2024-12-11 21:44:39 +0100

    Fixed issues with cl

--- project/tests.sh
+++ project/tests.sh
@@ -3,7 +3,7 @@
 echo "Running Automated Tests for the Data Pipeline..."
 
 # Run the tests
-python3 -m unittest discover -s project -p "tests.py"
+python3 ./project/tests.py
 
 # Wait for user input to prevent the terminal from closing
 echo "Press any key to exit."
